dcce05568440a7545a63f2dd0e69353f396a3911
Michał Siwek <mike21@aol.pl>
2013-10-19 21:29:58 +0200

    Add option to regulate accuracy

--- whistle.js
+++ whistle.js
@@ -26,12 +26,22 @@
       inputNode = null,
       analyser = null;
 
-  var whistleEvent = null;
+  var whistleEvent = null,
+      whistleReadyEvent = null;
 
   function Whistle() {
-    this.init = function(whistleEventName) {
+    this.init = function(whistleEventName, once, precision) {
       this.whistleEventName = whistleEventName || "whistle";
+      this.once = once || false;
+      
       this.whistling = null;
+      this.intensity = null;
+      
+      if(precision == "low") {
+        this.precision = 150;
+      } else {
+        this.precision = 250;
+      }
 
       navigator.getUserMedia({ audio: true }, startStream, function(error) {
         console.log("error: " + error);
@@ -39,6 +49,13 @@
 
       whistleEvent = new CustomEvent(
         this.whistleEventName, {
+          bubbles: true,
+          cancelable: true
+        }
+      );
+
+      whistleReadyEvent = new CustomEvent(
+        'whistleReady', {
           bubbles: true,
           cancelable: true
         }
@@ -64,6 +81,8 @@
     zeroGain.gain.value = 0.0;
     inputNode.connect(actx.destination);
 
+    document.dispatchEvent(whistleReadyEvent);
+    
     analyse();
   }
 
@@ -71,14 +90,17 @@
     var frequencies = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(frequencies);
     
-    for(var i=25; i<=80; ++i) {
-      if(frequencies[i] > 250) {
+    for(var i=29; i<=80; ++i) {
+      if(frequencies[i] > whistle.precision) {
         document.dispatchEvent(whistleEvent);
         whistle.whistling = true;
+        whistle.intensity = (i - 29) * 2;
       } else {
         whistle.whistling = false;
       }
     }
-    requestAnimationFrame(analyse);
+    if(!whistle.once) {
+      requestAnimationFrame(analyse);
+    }
   }
 })(window, document, navigator)
